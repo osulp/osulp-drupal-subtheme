0c44bfffd138ede2aa7a41eef47185e4e52c3613
KennaW <warsinsk@oregonstate.edu>
2024-08-27 18:35:56 -0700

    initial files

---
+++ js/osulp.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * OSULP behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.osulp = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
