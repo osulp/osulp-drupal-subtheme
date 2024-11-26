2ad70d37ed7b94512d9dde5b3b6bd05747720fbe
Corey Gillen <corey.gillen@oregonstate.edu>
2024-11-26 13:04:59 -0800

    Basic search bar

--- js/osulp.js
+++ js/osulp.js
@@ -15,3 +15,16 @@
   };
 
 } (Drupal));
+
+/* Search Bar */
+function handleSearchLocChange(elem) {
+  methodElem = document.getElementById("osulp-search-method");
+  locValue = elem.value;
+  showValues = ["cat", "cr"];
+  show = showValues.includes(locValue);
+
+  methodElem.classList.toggle("d-none", !show);
+}
+document.addEventListener("DOMContentLoaded", function () {
+  handleSearchLocChange(document.getElementById("osulp-search-loc"));
+});
